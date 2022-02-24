e7cfaa40f1af826cb4e32c3a102a44c78df1220a
Thibaut SCHURCH <thibaut@Thibauts-MacBook-Pro.local>
2022-02-24 15:56:51 +0100

    added files

---
+++ src/components/A.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import B from "./B.vue";
+
+const props = defineProps<{ count: number }>()
+
+</script>
+
+<template>
+    <B :count="props.count-1" />
+    <li>A</li>
+</template>
---
+++ src/components/B.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import A from './A.vue';
+
+const props = defineProps<{ count: number }>()
+
+</script>
+
+<template>
+    <A v-if="props.count > 0" :count="props.count-1" />
+    <li>B</li>
+</template>
